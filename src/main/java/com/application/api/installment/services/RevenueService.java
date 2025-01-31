7544a8e546ed285327de7506522f33d7f81066b7
vitoralveschaves <vitorchavesalv@gmail.com>
2025-01-31 14:55:33 -0300

    refactor: move logic to implementation class RevenueServiceImpl

--- src/main/java/com/application/api/installment/services/RevenueService.java
+++ src/main/java/com/application/api/installment/services/RevenueService.java
@@ -1,69 +1,12 @@
 package com.application.api.installment.services;
 
-import com.application.api.installment.entities.Installment;
 import com.application.api.installment.entities.Revenue;
-import com.application.api.installment.exceptions.RevenueNotFoundException;
-import com.application.api.installment.repositories.InstallmentRepository;
-import com.application.api.installment.repositories.RevenueRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.UUID;
 
-@Service
-@RequiredArgsConstructor
-public class RevenueService {
-
-    private final RevenueRepository revenueRepository;
-    private final InstallmentRepository installmentRepository;
-
-    @Transactional
-    public Revenue createRevenue(Revenue revenue) {
-        Revenue revenueSaved = revenueRepository.save(revenue);
-
-            var result = revenueSaved
-                    .getTotalValue()
-                    .divide(
-                            BigDecimal.valueOf(revenueSaved.getQuantityInstallments()),
-                            3,
-                            RoundingMode.HALF_EVEN
-                    );
-            for (int i = 0; i < revenueSaved.getQuantityInstallments(); i++) {
-                Installment installment = createInstallment(revenueSaved, i, result);
-                installmentRepository.save(installment);
-            }
-
-        return revenueSaved;
-    }
-
-    public Revenue getById(UUID id) {
-        return revenueRepository.findById(id)
-                .orElseThrow(() -> new RevenueNotFoundException("Despesa não encontrada"));
-    }
-
-    public void delete(UUID id) {
-        Revenue revenue = getById(id);
-        installmentRepository.deleteInstallmentByRevenue(revenue);
-
-        revenueRepository.delete(revenue);
-    }
-
-    public void update(Revenue revenue) {
-        revenueRepository.save(revenue);
-    }
-
-    private Installment createInstallment(Revenue revenueSaved, int i, BigDecimal result) {
-        Installment installment = new Installment();
-        installment.setCurrentMonth(revenueSaved.getInitialDate().plusMonths(i));
-        installment.setInstallmentNumber(i + 1);
-        installment.setInstallmentValue(result);
-        installment.setQuantityInstallments(revenueSaved.getQuantityInstallments());
-        installment.setPaid(false);
-        installment.setInitialDate(revenueSaved.getInitialDate());
-        installment.setRevenue(revenueSaved);
-        return installment;
-    }
+public interface RevenueService {
+    Revenue createRevenue(Revenue revenue);
+    Revenue getById(UUID id);
+    void delete(UUID id);
+    void update(Revenue revenue);
 }
