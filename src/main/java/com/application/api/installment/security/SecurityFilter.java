2c1d8f5c9d58aa524622dbc7ee16312dab908338
vitoralveschaves <vitorchavesalv@gmail.com>
2025-02-16 20:57:40 -0300

    feat: add SecurityFilter class

---
+++ src/main/java/com/application/api/installment/security/SecurityFilter.java
@@ -0,0 +1,41 @@
+package com.application.api.installment.security;
+
+import com.application.api.installment.entities.User;
+import com.application.api.installment.repositories.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private static final String AUTHORIZATION = "Authorization";
+
+    private final TokenService tokenService;
+    private final UserRepository userRepository;
+
+    public SecurityFilter(TokenService tokenService, UserRepository userRepository) {
+        this.tokenService = tokenService;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = request.getHeader(AUTHORIZATION);
+        if(token != null) {
+            String tokenWithoutBearer = token.replace("Bearer ", "");
+            String email = tokenService.validateToken(tokenWithoutBearer);
+            User user = userRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("User not found"));
+            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+        filterChain.doFilter(request, response);
+    }
+}
