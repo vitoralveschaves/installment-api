847c953812779fbf2dae8cd3dcbaa065cb4a41af
vitoralveschaves <vitorchavesalv@gmail.com>
2025-05-28 01:30:43 -0300

    refactor: refactor RevenueType enum

--- src/main/java/com/application/api/installment/enums/RevenueType.java
+++ src/main/java/com/application/api/installment/enums/RevenueType.java
@@ -1,6 +1,16 @@
 package com.application.api.installment.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum RevenueType {
-    INCOME,
-    INVESTMENT
+
+    INCOME("Income"),
+    INVESTMENT("Investment");
+
+    private final String description;
+
+    RevenueType(String description) {
+        this.description = description;
+    }
 }
