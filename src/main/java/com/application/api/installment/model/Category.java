7b6e91534ed9fe69e4724db106c52d49016f9ef0
vitoralveschaves <vitorchavesalv@gmail.com>
2025-05-28 01:30:26 -0300

    refactor: refactor Category imports

--- src/main/java/com/application/api/installment/model/Category.java
+++ src/main/java/com/application/api/installment/model/Category.java
@@ -1,7 +1,20 @@
 package com.application.api.installment.model;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
